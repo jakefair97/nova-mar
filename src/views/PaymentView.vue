09731321569d3bf0b4131521ad4ef9625ff885d6
jakefair <100549835+jakefair97@users.noreply.github.com>
2024-01-31 17:28:53 -0600

    most authentication works and name displays on logged in screen

--- src/views/PaymentView.vue
+++ src/views/PaymentView.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-  <!-- figure out how to use stripe and setup a payment screen -->
+  <!-- figure out how to use stripe and setup a payment screen 
+  every tenant owes rent on the first of the month; that amount varies by property (I assume)
+  so each property can have a 'rent' field attached to it which is how much rent costs
+  the tenant also needs a check to see if they paid rent; if they did, the amount owed shouled be $0
+  if they haven't paid, the amount should equal the cost of rent for that property
+  also would be cool to send an invoice on the first of the month to remind them that rent is due-->
  <h1>Pay up!</h1>
 </template>
 
