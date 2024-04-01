3868cd7b1917b740362b090439994f651b0455e6
jakefair <100549835+jakefair97@users.noreply.github.com>
2024-04-01 17:51:30 -0500

    modified firestore rules to allow reading of documents in database

--- src/views/LeaseView.vue
+++ src/views/LeaseView.vue
@@ -1,19 +1,23 @@
 <script setup>
 // something something import the file containing the resident's lease; should I make this an actual page? Yes because Allie said most tenants will be using this from their phone and having them download the file to look at it somewhere else won't work because that's an unnecessary step
 import { db } from '../firebase'
-import { useCollection, useDocument } from 'vuefire'
-import { collection, getDocs, query, doc } from 'firebase/firestore'
-const lease = useDocument(doc(collection(db, 'leases'), 'w2Uig3fdAbW1QW9h4jLz'))
-console.log(lease)
+import { useCollection, useDocument, useFirestore } from 'vuefire'
+import { collection, getDocs, getDoc, query, doc } from 'firebase/firestore'
+// const lease1 = useDocument(doc(collection(db, 'leases'), 'w2Uig3fdAbW1QW9h4jLz'))
+
+const leasesRef = collection(db, 'leases');
+const leases = useCollection(leasesRef);
+console.log(leases)
 </script>
 
 <template>
   <!-- Just need one element to display the lease -->
   <div class="lease">
     <h1>Your lease will be here</h1>
+    <div v-for="lease in leases" :key="index">
+        {{ lease.lease }}
+    </div>
 
-    <p>lease</p>
-    <p>{{ q }}</p>
   </div>
 </template>
 
