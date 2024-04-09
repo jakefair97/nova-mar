d1d3b79e3bbcad6818e803f0443b8e7ddb663d3f
jakefair <100549835+jakefair97@users.noreply.github.com>
2024-04-08 19:28:50 -0500

    lease displays as a pdf on the view lease page

--- src/views/LeaseView.vue
+++ src/views/LeaseView.vue
@@ -1,21 +1,30 @@
 <script setup>
 // something something import the file containing the resident's lease; should I make this an actual page? Yes because Allie said most tenants will be using this from their phone and having them download the file to look at it somewhere else won't work because that's an unnecessary step
 import { db } from '../firebase'
-import { useCollection, useDocument, useFirestore } from 'vuefire'
+import { useDocument, useFirebaseStorage, useStorageFile } from 'vuefire'
 import { collection, getDocs, getDoc, query, doc } from 'firebase/firestore'
+import { ref } from 'firebase/storage';
 // const lease1 = useDocument(doc(collection(db, 'leases'), 'w2Uig3fdAbW1QW9h4jLz'))
 
-const leasesRef = collection(db, 'leases');
-const leases = useCollection(leasesRef);
-console.log(leases)
+const myLease = useDocument(doc(db, 'leases', 'w2Uig3fdAbW1QW9h4jLz'))
+const storage = useFirebaseStorage();
+const leaseRef = ref(storage, 'leases/lease-practice.pdf');
+
+const { url } = useStorageFile(leaseRef);
+
 </script>
 
 <template>
   <!-- Just need one element to display the lease -->
   <div class="lease">
     <h1>Your lease will be here</h1>
-    <div v-for="lease in leases" :key="index">
+    <!-- <div v-for="lease in leases" :key="index">
         {{ lease.lease }}
+    </div> -->
+    <div>
+        <!-- {{ myLease.lease }} -->
+        <!-- {{ lease }} -->
+        <iframe v-if="url" :src="url" width="100%" height="500" />
     </div>
 
   </div>
